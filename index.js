ac4fa984d0bfe7596bbe26455c3f4ba3b3bbe106
KorotchenkoSV <09z1p23k@mail.ru>
2025-12-29 23:26:10 +0700

    part 1

---
+++ index.js
@@ -0,0 +1,7 @@
+const genDiff = (filepath1, filepath2) => {
+  // Здесь будет логика сравнения файлов
+  // Пока возвращаем заглушку
+  return `Difference between ${filepath1} and ${filepath2}`;
+};
+
+export default genDiff;
