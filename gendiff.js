6752d78bcd6b43fe490e54278841b4f6f48d5b2e
KorotchenkoSV <09z1p23k@mail.ru>
2025-12-30 21:56:13 +0700

    part 3

--- gendiff.js
+++ gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import parseFile from './src/parse.js';
 
 program
   .name('gendiff')
@@ -8,18 +9,19 @@ program
   .version('1.0.0', '-V, --version', 'output the version number')
   .argument('<filepath1>', 'path to the first configuration file')
   .argument('<filepath2>', 'path to the second configuration file')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    try {
+      const data1 = parseFile(filepath1);
+      const data2 = parseFile(filepath2);
+      
+      console.log('File 1 data:', data1);
+      console.log('File 2 data:', data2);
+      console.log('Format:', options.format);
+    } catch (error) {
+      console.error('Error:', error.message);
+      process.exit(1);
+    }
+  });
 
 program.parse();
-
-// Для демонстрации: выводим полученные аргументы и опции
-const options = program.opts();
-const [filepath1, filepath2] = program.args;
-
-console.log('Arguments:');
-console.log(`  filepath1: ${filepath1}`);
-console.log(`  filepath2: ${filepath2}`);
-
-if (options.format) {
-  console.log(`Format: ${options.format}`);
-}
